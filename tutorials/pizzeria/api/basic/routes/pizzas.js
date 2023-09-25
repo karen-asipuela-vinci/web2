608fc3eb8f9c827fc66f6813c24136df25b15258
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2023-09-25 14:46:06 +0200

    exemple cours - pizzas.js non complet

--- tutorials/pizzeria/api/basic/routes/pizzas.js
+++ tutorials/pizzeria/api/basic/routes/pizzas.js
@@ -30,9 +30,46 @@ const MENU = [
 ];
 
 // Read all the pizzas from the menu
-router.get('/', (req, res, next) => {
-  console.log('GET /pizzas');
-  res.json(MENU);
+/* Read all the pizzas from the menu
+   GET /pizzas?order=title : ascending order by title
+   GET /pizzas?order=-title : descending order by title
+*/
+// Create a pizza to be added to the menu.
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const content = req?.body?.content?.length !== 0 ? req.body.content : undefined;
+
+  console.log('POST /pizzas');
+
+  if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+
+  const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newPizza = {
+    id: nextId,
+    title: title,
+    content: content,
+  };
+
+  MENU.push(newPizza);
+
+  res.json(newPizza);
+});
+
+
+
+
+// Read the pizza identified by an id in the menu
+router.get('/:id', (req, res) => {
+  console.log(`GET /pizzas/${req.params.id}`);
+
+  const indexOfPizzaFound = MENU.findIndex((pizza) => pizza.id == req.params.id);
+
+  if (indexOfPizzaFound < 0) return res.sendStatus(404);
+
+  res.json(MENU[indexOfPizzaFound]);
 });
 
 module.exports = router;
