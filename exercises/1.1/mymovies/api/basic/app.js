f47279d473991ed37980178331752f314252bef1
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2023-09-25 14:20:58 +0200

    modifation app.js

--- exercises/1.1/mymovies/api/basic/app.js
+++ exercises/1.1/mymovies/api/basic/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { nextTick } = require('process');
 
 var app = express();
 
@@ -13,6 +14,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//creation tableau
+
+app.use((req, res, next) => {
+    let methodIndex = `${req.path} ${req.method}` ; //permet de récupérer les variables html
+});
+
+app.use((req, res, next) => {
+    console.log('GET/ : ', req.path);
+    next();
+  });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
